97f157df9a4655d2a1b3e28c0c66c7cb664fc2e5
= <jmeunier@bu.edu>
2017-09-09 14:32:31 -0700

    final cleanup

--- src/main/java/codechallenge/Main.java
+++ src/main/java/codechallenge/Main.java
@@ -6,6 +6,6 @@ package main.java.codechallenge;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Starting up server..");
+        System.out.println("Starting up server...");
     }
 }
