7279d2be585fb034e5991344be771c556b6893bc
= <jmeunier@bu.edu>
2017-09-09 14:29:32 -0700

    cleaning up initial setup

---
+++ src/main/java/codechallenge/Main.java
@@ -0,0 +1,11 @@
+package main.java.codechallenge;
+
+/**
+ * Created by jmeunier28 on 9/9/17.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Starting up server..");
+    }
+}
